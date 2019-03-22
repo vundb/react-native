2e1e6b03da35d488a4e2b6b11b0eaae0eee5fd38
Viktor Breitenbücher <vic.b@outlook.com>
2019-03-22 14:37:09 +0700

    Initial Commit.

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
+}
